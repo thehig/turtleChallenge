a6cdf4269e7c4aff82b9447a79c7d372e9e9eb10
David Higgins <thehig@gmail.com>
2017-10-21 16:46:07 +0100

    Outline the remaining tests I'd like to run

--- scratchFile.ts
+++ scratchFile.ts
@@ -17,3 +17,45 @@ try {
     console.log(e);
 }
 
+
+// describe "App" ->
+// it "takes 2 CLI parameters, the initial state (JSON file) and the actions (txt file)"
+// it "handles thrown errors cleanly"
+// it "returns a string of final halting state and number of commands executed"
+
+// describe "File IO", ->
+// it "reads a settings (json) file from a given path"
+// it "reads an actions (txt) file from a given path"
+// it "throws appropriate errors"
+//   * File not Found
+//   * Invalid permissions/access
+
+// describe "GameRunner", ->
+// it "should take a GameState as its constructor parameter"
+
+// describe "Tick" ->
+//   it "should take an Action as a parameter"
+//   it "should advance the internal GameState if not halted"
+//   it "should not advance the internal GameState if halted"
+
+// describe "Run" ->
+//   it "should take Actions as a parameter"
+//   it "should iteratively tick through the Actions"
+//   it "should return the output of those Actions on a given GameState"
+  
+//   it "halts when turtle state becomes Success, MineHit, OutOfBounds"
+//   it "halts when all Actions have been taken"
+//   it "continues processing Actions until halt"
+
+//   it "returns an object or string describing the final state"
+//     * Success, MineHit, OutOfBounds, InDanger
+//     * Number of actions taken
+
+// describe "GameState"
+//     it "errors if no Turtle is provided"
+//     it "errors if no Exit is provided"
+
+// describe "Board dimensions supported"
+//     it "1x1"
+//     it "1x10"
+//     it "10x1"
