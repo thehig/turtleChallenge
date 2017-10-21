4eed761d7498816761959d75e1359e858ee4e503
David Higgins <thehig@gmail.com>
2017-10-21 20:37:35 +0100

    App running correctly for given inputs

--- app.js
+++ app.js
@@ -1,23 +1,29 @@
 const fs = require('fs');
-const { TurtleChallenge, Actions, Action } = require('./src');
+const { TurtleChallenge, GameState, Actions, Action } = require('./build');
 
 // Read state json file
-const state = JSON.parse(fs.readFileSync('./games/3x3/1.json'));
+const initial = JSON.parse(fs.readFileSync('./games/3x3/1.json'));
+/* 
+// Run solution step by step
+const gameInstance = new TurtleChallenge(new GameState(initial.board, initial.turtle, initial.exit, initial.mines));
+gameInstance.tick(Action.Rotate);
+gameInstance.tick(Action.Move);
+gameInstance.tick(Action.Move);
+gameInstance.tick(Action.Rotate);
+gameInstance.tick(Action.Move);
+gameInstance.tick(Action.Move);
+console.log(JSON.stringify(gameInstance, null, 4));
+*/
+
+
+// Run all solutions in file, separated by \r\n
 // Read actions txt file
 const actions = fs.readFileSync('./games/3x3/1.txt', 'utf8').trim().split('\r\n');
 
-
-const gameInstance = new TurtleChallenge(state);
-console.log(JSON.stringify(gameInstance, null, 4));
-gameInstance.tick(Action.Move);
-console.log(JSON.stringify(gameInstance, null, 4));
-/* 
-// Run all solutions
 for(var i = 0; i < actions.length; i++) {
     // Create TurtleChallenge
     try {
-        console.log(state);
-        const gameInstance = new TurtleChallenge(state);
+        const gameInstance = new TurtleChallenge(new GameState(initial.board, initial.turtle, initial.exit, initial.mines));
         const outcome = gameInstance.run(new Actions(actions[i]));
         // Run Actions
         console.log(`${actions[i]}: ${outcome}`);
@@ -26,4 +32,3 @@ for(var i = 0; i < actions.length; i++) {
         console.log(err);
     }
 }
- */
